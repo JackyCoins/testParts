8d294b5436470bce50ecd713600a5898190eb0df
Nikita Rusanov <rusanov.n.d@gmail.com>
2018-07-11 23:38:52 +0300

    Added example for react's context

--- src/react/context/index.js
+++ src/react/context/index.js
@@ -2,8 +2,57 @@
 import React from "react";
 import { render } from "react-dom";
 
-const App = () => (
-  <div>App</div>
+const themes = {
+  light: {
+    color: "#000",
+    background: "#fff"
+  },
+  dark: {
+    color: "#fff",
+    background: "#000"
+  }
+};
+
+const ThemeContext = React.createContext({
+  theme: themes.light,
+  toggleTheme: () => {}
+});
+
+const ThemedButton = props => (
+  <ThemeContext.Consumer>
+    {({ theme, toggleTheme }) => (
+      <button
+        onClick={toggleTheme}
+        style={{ color: theme.color, background: theme.background }}
+      >
+        Toggle theme
+      </button>
+    )}
+  </ThemeContext.Consumer>
 );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggleTheme = () =>
+      this.setState(prevState => ({
+        theme: prevState.theme === themes.light ? themes.dark : themes.light
+      }));
+
+    this.state = {
+      theme: themes.light,
+      toggleTheme: this.toggleTheme
+    };
+  }
+
+  render() {
+    return (
+      <ThemeContext.Provider value={this.state}>
+        <ThemedButton />
+      </ThemeContext.Provider>
+    );
+  }
+}
 
 render(<App />, document.getElementById("root"));
