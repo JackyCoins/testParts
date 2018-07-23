417064c91c96101913de958d67761c1a88655b51
Nikita Rusanov <rusanov.n.d@gmail.com>
2018-07-23 22:27:30 +0300

    Created first styled components

--- src/styles/css-in-js/styled-components/index.js
+++ src/styles/css-in-js/styled-components/index.js
@@ -1,15 +1,30 @@
 //region Import libraries
 import React from "react";
 import { render } from "react-dom";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 //endregion
 
-const Button = styled.button``;
+const Button = styled.button`
+  border-radius: 3px;
+  padding: 0.25em 1em;
+  margin: 0 1em;
+  background: transparent;
+  color: palevioletred;
+  border: 2px solid palevioletred;
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+    `};
+`;
 
 const App = props => (
-  <div>App</div>
+  <div>
+    <Button>Button</Button>
+    <Button primary>Primary Button</Button>
+  </div>
 );
 
 render(<App />, document.getElementById("root"));
-
-
