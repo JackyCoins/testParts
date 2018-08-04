3530c1f82e68282318d5203bb508093dc8e6e2bf
Nikita Rusanov <rusanov.n.d@gmail.com>
2018-08-04 20:46:12 +0300

    Added story about button

--- src/tools/storybook/index.stories.js
+++ src/tools/storybook/index.stories.js
@@ -1,10 +1,14 @@
 //region Import libraries
 import React from "react";
-import { storiesOf } from '@storybook/react';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
 //endregion
 
 //region Import components
 import App from "./App";
+import Button from "./Button";
 //endregion
 
-storiesOf("Example storybook", module).add("App", () => <App />);
+storiesOf("Example storybook", module)
+  .add("App", () => <App />)
+  .add("Button", () => <Button onClick={action("clicked")}>Button</Button>);
